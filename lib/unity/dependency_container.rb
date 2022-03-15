f4d59a993654490b290d399a25c9684e9435031c
saluzafa <julien@pocketsizesun.com>
2022-03-15 12:42:46 +0100

    bump version to v1.1.0 [feature] make lib thread-safe

--- lib/unity/dependency_container.rb
+++ lib/unity/dependency_container.rb
@@ -11,11 +11,18 @@ module Unity
     end
 
     def di_instances
-      @di_instances ||= {}
+      Thread.current[:di_instances]
     end
 
     def di(name)
-      di_instances[name] ||= di_container_repository[name].call
+      return di_instances[name] if !di_instances.nil? && di_instances.key?(name)
+
+      Thread.current[:di_instances] ||= {}
+
+      unless di_container_repository.key?(name)
+        raise Error, "Dependency '#{name}' does not exists"
+      end
+      Thread.current[:di_instances][name] = di_container_repository[name].call
     end
 
     def dependency(name, &block)
